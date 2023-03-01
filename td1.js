30864e226a99af10d9cd30c9baf135721e619dce
FrirexD <frirex.go@gmail.com>
2023-03-01 22:04:07 +0100

    Menu & colors

--- td1.js
+++ td1.js
@@ -1,7 +1,21 @@
 const fs = require("fs"); 
+const { abort } = require("process");
 let rawdata = fs.readFileSync("users.json","utf8");
 // eslint-disable-next-line no-undef
-var args = process.argv;
+//var args = process.argv;
+
+//Pour les inputs de Keybords
+const readline = require("readline");
+// eslint-disable-next-line no-undef
+readline.emitKeypressEvents(process.stdin);
+// eslint-disable-next-line no-undef
+if(process.stdin.isTTY) process.stdin.setRawMode(true);
+Menuprint();
+// Listen to Keypress 
+// eslint-disable-next-line no-undef
+process.stdin.on("keypress", (str, key)=> {  if(key.name == "1") { CallMenu(1); } if(key.name == "2" ) { CallMenu(2); } if(key.name == "q"){process.abort(); } } );
+
+
 
 
 
@@ -30,79 +44,97 @@ function inCompanies(companytable, string){
     }
     return false;
 }
-
-// pour les country
-if(args[2] == "country"){
-    //pour chaque élément de notre liste 
-    for(let i = 0; i < data.length; i++){
-        //on ajoute le contry dans notre tableau lorsqu'il ne fait pas partie du tableau 
-        // On regarde s'il en fait partie avec notre fonction inCountries
-        if(!inCountries(countries,data[i].country)){
-            let obj = {Country:data[i].country, Count:1};
-            countries.push(obj);
-        }
-        //si le Country fait déjà parti du tableau alors on incrémente de 1 notre Objet
-        // on incrémente avec Count
-        else{   
-            let j = 0;
-            while(countries[j].Country != data[i].country){ 
-                j++;
+function CallMenu( k )
+{
+    // pour les country
+    if(  k == 1 ){ //if keypress = 1  
+        //pour chaque élément de notre liste 
+        for(let i = 0; i < data.length; i++){
+            //on ajoute le contry dans notre tableau lorsqu'il ne fait pas partie du tableau 
+            // On regarde s'il en fait partie avec notre fonction inCountries
+            if(!inCountries(countries,data[i].country)){
+                let obj = {Country:data[i].country, Count:1};
+                countries.push(obj);
             }
-            countries[j].Count++ ;
+            //si le Country fait déjà parti du tableau alors on incrémente de 1 notre Objet
+            // on incrémente avec Count
+            else{   
+                let j = 0;
+                while(countries[j].Country != data[i].country){ 
+                    j++;
+                }
+                countries[j].Count++ ;
+            }
         }
-    }
-    
-    //fonction de tri Pour avoir dans l'ordre décroissant
-    for(let i = 0;i < countries.length;i++){        
-        let tmp = countries[i];
-        let j = i-1;
-        while((j>=0) && (countries[j].Count < tmp.Count)){
-            countries[j+1] = countries[j];
-            j--;
+        
+        //fonction de tri Pour avoir dans l'ordre décroissant
+        for(let i = 0;i < countries.length;i++){        
+            let tmp = countries[i];
+            let j = i-1;
+            while((j>=0) && (countries[j].Count < tmp.Count)){
+                countries[j+1] = countries[j];
+                j--;
+            }
+            countries[j+1] = tmp;
         }
-        countries[j+1] = tmp;
+
+        //Printing final elements
+        console.log(countries);
     }
 
-    //Printing final elements
-    console.log(countries);
+    //pour les companies on utilise la meme méthode
+    //on regarde que l'argument est bien Company
+    else if(k == 2){ // keypress = 2 
+        //pour chaque élément de notre liste
+        for(let i = 0; i < data.length; i++){
+            //si la company ne fait pas partie de notre tableau on l'ajoute avec un coupteur dans un objet 
+            //On regarde si la company ne fait pas partie de notre tableau avec la fonction inCompanies
+            if(!inCompanies(companies,data[i].company)){
+                let obj = {Company:data[i].company, Count:1};
+                companies.push(obj);
+            }
+            // si la company fait déjà partie de notre tableau alors on incrémente son compteur
+            else{   
+                let j = 0;
+                while(companies[j].Company != data[i].company){ 
+                    j++;
+                }
+                companies[j].Count++ ;
+            }
+        }
+        
+        //tri des objets dans notre tableau dans l'ordre décroissant
+        for(let i = 0;i < companies.length;i++){ 
+            let tmp = companies[i];
+            let j = i-1;
+            while((j>=0) && (companies[j].Count < tmp.Count)){
+                companies[j+1] = companies[j];
+                j--;
+            }
+            companies[j+1] = tmp;
+        }
+
+        //print les companies
+        console.log(companies);
+    }
+    else{
+        //dans le cas ou la commande rentré dans ARGV n'est pas company ou country
+        console.log("ERROR: no option specified (type company or country)");
+    }
+    Menuprint();
+
 }
 
-//pour les companies on utilise la meme méthode
-//on regarde que l'argument est bien Company
-else if(args[2] == "company"){
-    //pour chaque élément de notre liste
-    for(let i = 0; i < data.length; i++){
-        //si la company ne fait pas partie de notre tableau on l'ajoute avec un coupteur dans un objet 
-        //On regarde si la company ne fait pas partie de notre tableau avec la fonction inCompanies
-        if(!inCompanies(companies,data[i].company)){
-            let obj = {Company:data[i].company, Count:1};
-            companies.push(obj);
-        }
-        // si la company fait déjà partie de notre tableau alors on incrémente son compteur
-        else{   
-            let j = 0;
-            while(companies[j].Company != data[i].company){ 
-                j++;
-            }
-            companies[j].Count++ ;
-        }
-    }
-    
-    //tri des objets dans notre tableau dans l'ordre décroissant
-    for(let i = 0;i < companies.length;i++){ 
-        let tmp = companies[i];
-        let j = i-1;
-        while((j>=0) && (companies[j].Count < tmp.Count)){
-            companies[j+1] = companies[j];
-            j--;
-        }
-        companies[j+1] = tmp;
-    }
 
-    //print les companies
-    console.log(companies);
-}
-else{
-    //dans le cas ou la commande rentré dans ARGV n'est pas company ou country
-    console.log("ERROR: no option specified (type company or country)");
+function Menuprint ()
+{
+    console.log("\n__________________");
+    console.log(" \n Menu :");
+    console.log("__________________");
+    console.log("press those keys to acces : ");
+    console.log("\x1b[36m%s\x1b[0m ","1 --> Countries ");
+    console.log("\x1b[33m%s\x1b[0m","2 --> Companies ");
+    console.log("\x1b[31m%s\x1b[0m","q --> QUIT ");
+    console.log("__________________");
+    
 }
